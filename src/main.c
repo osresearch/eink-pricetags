4ed49ac5760adebe5e124a3f63bbf1f8ba3ecdb8
Trammell Hudson <hudson@trmm.net>
2022-04-20 13:45:53 +0200

    5x7 font from the model100 project

--- src/main.c
+++ src/main.c
@@ -2,14 +2,8 @@
 #include <stdint.h>
 #include "epd.h"
 
-int main(void)
+static void epd_checkerboard(void)
 {
-	WDTCTL = WDTPW + WDTHOLD; // Stop WDT
-
-	epd_setup();
-	epd_reset();
-	epd_init();
-
 	epd_draw_start();
 
 /*
@@ -26,6 +20,63 @@ int main(void)
 				epd_data(y);
 		}
 
+	epd_draw_end();
+}
+
+static const char msg[] = "Helloworld!";
+static unsigned msg_i = 0;
+static uint8_t glyph_x = 0;
+static uint8_t msg_x = 0;
+extern const uint8_t font[][6];
+
+static uint8_t bitmap(unsigned y, unsigned x)
+{
+	const char c = msg[msg_i];
+	if (c == '\0')
+	{
+		msg_i = 0;
+		return 0xFF;
+	}
+
+	if (msg_x != x)
+	{
+		msg_x = x;
+		glyph_x++;
+	}
+
+	if (glyph_x == (6 << 2))
+	{
+		glyph_x = 0;
+		msg_i++;
+		return 0xFF;
+	}
+
+	const uint8_t * const glyph = font[c - 0x20];
+	const uint8_t bitmap = glyph[glyph_x >> 2];
+
+	const uint8_t bit = (bitmap << ((y >> 4) & 7)) & 0x80;
+
+	return bit ? 0x00 : 0xFF;
+}
+
+int main(void)
+{
+	WDTCTL = WDTPW + WDTHOLD; // Stop WDT
+
+	epd_setup();
+	epd_reset();
+	epd_init();
+
+	if (0)
+	epd_checkerboard();
+
+	// try to draw some text...
+	epd_draw_start();
+	for(unsigned y = 0 ; y < EPD_HEIGHT ; y++)
+		for(unsigned x = 0 ; x < EPD_WIDTH ; x += 8)
+		{
+			epd_data(bitmap(x,y));
+		}
 	epd_draw_end();
 
 	epd_shutdown();
