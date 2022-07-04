72e1319c82b3b89b680304e96c59faacadc9473f
Trammell Hudson <hudson@trmm.net>
2022-07-04 22:36:38 +0200

    main: poll gateway for new image data, write it to the flash

--- src/main.c
+++ src/main.c
@@ -9,33 +9,6 @@
 static volatile uint32_t timer;
 
 
-static void epd_checkerboard(void)
-{
-	epd_draw_start();
-
-/*
- * 1 == white, 0 == black
- * draw order is X first (122 across), 8 pixels at a time.
- * can change this with the incrment?
- */
-	for(unsigned y = 0 ; y < EPD_HEIGHT ; y++)
-		for(unsigned x = 0 ; x < EPD_WIDTH ; x += 8)
-		{
-			if (x < 64 && y < 32)
-				epd_data(0x00);
-			else
-				epd_data(0xFF);
-/*
-			if (((x >> 4) & 1) ^ ((y >> 4) & 1))
-				epd_data(0xFF);
-			else
-				epd_data(y);
-*/
-		}
-
-	epd_display();
-}
-
 static char msg[16];
 static unsigned msg_i = 0;
 static uint8_t glyph_x = 0;
@@ -122,43 +95,96 @@ static char hexdigit(unsigned x)
 }
 
 typedef struct {
-	uint8_t controller[4];
-	uint8_t id[4];
+	uint8_t len;
+	uint32_t gw_id;
+	uint32_t tag_id;
 	uint8_t channel;
 } radio_config_t;
 
-static const radio_config_t config = {
-	.controller = { 0x55, 0xab, 0xcd, 0xef },
-	.id = { 0x50, 0x12, 0x34, 0x56 },
+const radio_config_t config = {
+	.gw_id = 0x55abcdef,
+	.tag_id = 0x50123456,
 	.channel = 4,
 };
 
+typedef struct {
+	uint8_t len;
+	uint32_t tag_id;
+	uint32_t img_id;
+	uint16_t offset; // in byte multiples
+}
+__attribute__((__packed__))
+msg_hello_t;
+
+typedef struct {
+	uint32_t img_id;
+	uint16_t offset;
+	uint8_t data[58];
+}
+__attribute__((__packed__))
+msg_data_t;
+
+
+static uint32_t img_id = 0;
+static uint32_t img_offset = 0;
+
+static uint8_t msg_buf[64];
+
+int check_for_updates(uint32_t flash_addr)
+{
+	msg_hello_t * const hello = (void*) msg_buf;
+	hello->len = sizeof(*hello);
+	hello->tag_id = config.tag_id;
+	hello->img_id = img_id;
+	hello->offset = img_offset;
+
+	// send a ping?
+	radio_tx(config.gw_id, (const void*) hello, 64); // sizeof(msg));
+
+	// wait for a reply
+	msg_data_t * const reply = (void *) msg_buf;
+	if (radio_rx(config.tag_id, (void*) reply, 64 /*sizeof(reply)*/, 10000) != 1)
+		return -1;
+
+	// we have data!
+	if (reply->img_id != img_id)
+	{
+		// starting a new image
+		img_id = reply->img_id;
+		img_offset = 0;
+		flash_erase(flash_addr);
+	}
+
+	// unexpected packet? discard it
+	if (reply->offset != img_offset)
+		return 0;
+
+	flash_write(flash_addr + img_offset, reply->data, sizeof(reply->data));
+	img_offset += sizeof(reply->data);
+
+#define IMG_SIZE 4096
+
+	if (img_offset >= IMG_SIZE)
+		return 1;
+
+	return 0;
+}
 
 int main(void)
 {
+	const uint32_t flash_addr = 0;
+
 	WDTCTL = WDTPW + WDTHOLD; // Stop WDT
 
 	// read the image from flash
 	flash_init();
-
-	draw_flash(0);
+	draw_flash(flash_addr);
 
 	// configure the radio, then let it turn off again
 	radio_init(config.channel);
 	radio_sleep();
 
-	static const uint8_t tx_msg[] = "\x40This is a test message";
-
-	while(1)
-	{
-		radio_tx(config.controller, tx_msg, 64);
-		radio_sleep();
-		for(volatile int i = 0 ; i < 1000 ; i++)
-			for(volatile int j = 0 ; j < 1000 ; j++)
-				;
-	}
-
-
+	// setup the watchdog to trigger every second
 	BCSCTL3 = LFXT1S_2; // select VLO as the ACLK (used by the WDT)
 	WDTCTL = WDT_ADLY_1000; // sleep for one second
 	IE1 |= WDTIE;
@@ -168,26 +194,41 @@ int main(void)
 	{
 		LPM3;
 
+/*
 		// every so often, check in with the head node
 		if ((timer & 0x3) != 0)
 			continue;
+*/
 
-		radio_tx(config.controller, tx_msg, 64);
+		while(1)
+		{
+			int status = check_for_updates(flash_addr);
+
+			if (status == -1)
+				break;
+
+			if (status == 1)
+				draw_flash(flash_addr);
+		}
+
+		radio_sleep();
+/*
 
 		short i = 0;
 		msg[i++] = '=';
-		msg[i++] = hexdigit(timer >> 28);
-		msg[i++] = hexdigit(timer >> 24);
-		msg[i++] = hexdigit(timer >> 20);
-		msg[i++] = hexdigit(timer >> 16);
-		msg[i++] = hexdigit(timer >> 12);
-		msg[i++] = hexdigit(timer >>  8);
-		msg[i++] = hexdigit(timer >>  4);
-		msg[i++] = hexdigit(timer >>  0);
+		msg[i++] = hexdigit(img_id >> 28);
+		msg[i++] = hexdigit(img_id >> 24);
+		msg[i++] = hexdigit(img_id >> 20);
+		msg[i++] = hexdigit(img_id >> 16);
+		msg[i++] = hexdigit(img_id >> 12);
+		msg[i++] = hexdigit(img_id >>  8);
+		msg[i++] = hexdigit(img_id >>  4);
+		msg[i++] = hexdigit(img_id >>  0);
 		msg[i++] = '!';
 
 		glyph_x = msg_i = msg_x = 0;
 		draw_msg();
+*/
 	}
 }
 
